148f009ce22e25f3c5e31ee91b36c85923ab42d2
kskshaf <118548920+kskshaf@users.noreply.github.com>
2025-06-24 13:06:48 +0800

    优化&修正
    完善&修正网络连接测试

--- ddns_update.sh
+++ ddns_update.sh
@@ -20,41 +20,69 @@ Domain_Record="ddns.example.com"
 api_v4="https://ipv4.ddnsip.cn"
 api_v6="https://ipv6.ddnsip.cn"
 
+# 网关地址
+gateway='192.168.0.1'
+
+# cloudflare 重连计数
+retry_count=0
+
+# 网关测试
+gateway_test() {
+    ping -c 2 $gateway &> /dev/null
+    if [ $? -ne 0 ]; then
+        ping -c 5 $gateway &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "网关无法连接"
+            exit 1
+        fi
+    fi
+}
+
 # 为了防止在 rc.local 中运行脚本时,网络尚未完全启动,造成获取 IP 失败,延迟 120 秒执行
 echo "延迟 120 秒执行"
 sleep 120
+gateway_test
 
 # 获取路由器/光猫的公网 IP
 # 为防止大量请求 API , 使用两个文件保存旧的 IP 地址
-IPv4_File=$HOME/.IPv4.addr && curl -s4m8 $api_v4 -k > $IPv4_File
-IPv6_File=$HOME/.IPv6.addr && curl -s6m8 $api_v6 -k > $IPv6_File
-IPv4=`cat $IPv4_File`
-IPv6=`cat $IPv6_File`
+IPv4_File=$HOME/.IPv4.addr && IPv4=$(curl -s4m8 $api_v4 -k)
+IPv6_File=$HOME/.IPv6.addr && IPv6=$(curl -s6m8 $api_v6 -k)
+echo $IPv4 > $IPv4_File
+echo $IPv6 > $IPv6_File
 
 # copy & paste from https://www.cnblogs.com/osnosn/p/11813096.html
 sys_ipv4() {
-    ip addr show|grep -A1 'inet [^f:]'|sed -nr 's#^ +inet ([0-9.]+)/[0-9]+ brd [0-9./]+ scope global .*#\1#p'
+    ip -4 addr show|grep -A1 'inet [^f:]'|sed -nr 's#^ +inet ([0-9.]+)/[0-9]+ brd [0-9./]+ scope global .*#\1#p'
 }
 
 sys_ipv6() {
-    ip addr show|grep -v deprecated|grep -v mngtmpaddr|grep -A1 'inet6 [^f:]'|grep -v ^--|sed -nr ':a;N;s#^ +inet6 ([a-f0-9:]+)/.+? scope global .*? valid_lft ([0-9]+sec) .*#\2 \1#p;Ta'|sort -nr|head -n1|cut -d' ' -f2
+    ip -6 addr show|grep -v deprecated|grep -v mngtmpaddr|grep -A1 'inet6 [^f:]'|grep -v ^--|sed -nr ':a;N;s#^ +inet6 ([a-f0-9:]+)/.+? scope global .*? valid_lft ([0-9]+sec) .*#\2 \1#p;Ta'|sort -nr|head -n1|cut -d' ' -f2
 }
 
 # 判断路由器/光猫拨号获取的 IP 地址是公网 IP 还是私网 IP , 如果 IPv4/IPv6 某项为空,说明是单栈
-if [ -n "$IPv4" ] && ! [[ `sys_ipv4` =~ $IPv4 ]]; then
-    echo -e "\e[33m路由器/光猫 PPPoE 获取的 IPv4 地址为私网IP! \e[0m"
-    IPv4_IsLAN="1"
+if [ -n "$IPv4" ]; then
+    if ! [[ `sys_ipv4` =~ $IPv4 ]]; then
+        echo -e "\e[33m路由器/光猫 PPPoE 获取的 IPv4 地址为私网IP! \e[0m"
+        IPv4_IsLAN="1"
+    else
+        echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv4 地址为公网IP! \e[0m"
+        IPv4_IsLAN="0"
+    fi
 else
-    echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv4 地址为公网IP! \e[0m"
-    IPv4_IsLAN="0"
+    echo -e "\e[32m网络错误，无法获取到外网 IPv4 地址! \e[0m"
+    exit 1
 fi
 
-if [ -n "$IPv6" ] && ! [[ `sys_ipv6` =~ $IPv6 ]]; then
-    echo -e "\e[33m路由器/光猫 PPPoE 获取的 IPv6 地址为私网IP! \e[0m"
-    IPv6_IsLAN="1"
+if [ -n "$IPv6" ]; then
+    if ! [[ `sys_ipv6` =~ $IPv6 ]]; then
+        echo -e "\e[33m路由器/光猫 PPPoE 获取的 IPv6 地址为私网IP! \e[0m"
+        IPv6_IsLAN="1"
+    else
+        echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv6 地址为公网IP! \e[0m"
+        IPv6_IsLAN="0"
+    fi
 else
-    echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv6 地址为公网IP! \e[0m"
-    IPv6_IsLAN="0"
+    echo -e "\e[32m无法获取到 IPv6 地址! \e[0m"
 fi
 
 update_IP() {
@@ -64,9 +92,14 @@ update_IP() {
     Record_Info=$(curl -s -X GET "$Record_Info_Api" -H "Authorization: Bearer $Cloudflare_API_Tokens" -H "Content-Type:application/json")
     Record_Info_Success=$(echo "$Record_Info" | jq -r ".success")
 
-    # 尝试重连至成功为止
-    while [ 0 -eq 0 ]
-    do
+    # 尝试重连10次，大于10次直接退出
+    while true; do
+        ((retry_count++))
+        if ((retry_count > 10)); then
+            echo -e "\e[31m重试次数大于10次， 退出进程\e[0m"
+            exit 1
+        fi
+
         if [[ $Record_Info_Success != "true" ]]; then
             echo -e "\e[31m与 Cloudflare 连接失败， 重试中……\e[0m"
             sleep 18
@@ -120,29 +153,35 @@ first_check() {
     fi
 }
 
-first_check
-
 check_ip_changes() {
     # 判断 IP 地址是否发生变化.如果IP发生变化,将新的IP地址写入文件,同时将IP赋值给New_IP变量,调用 update_IP 函数更新 IP
     # $IPv4/$IPv6 为空时说明路由器/光猫没有 IPv4/IPv6 地址,不予处理.
     # $IPv4_IsLAN/$IPv6_IsLAN 的值为 1 ,说明路由器/光猫获取的 IP 为内网 IP ,不予处理.
     # $(ip add show) 不包含 $(cat $IPv4_File) ,说明 IP 已发生变化.
+    gateway_test
+
+    if [[ -n `sys_ipv6` ]] && ! [ -n "$IPv6" ]; then
+        IPv6=$(curl -s6m8 $api_v6 -k)
+        echo -e "\e[32m已重新获取到 IPV6 地址：$IPv6\e[0m"
+    fi
+
     if [ -n "$IPv4" ] && [ "$IPv4_IsLAN" != "1" ] && ! [[ `sys_ipv4` =~ `cat $IPv4_File` ]]; then
-        curl -s4m8 $api_v4 -k > $IPv4_File
-        New_IP=`cat $IPv4_File`
+        New_IP=$(curl -s4m8 $api_v4 -k) && echo $New_IP > $IPv4_File
         echo -e "\e[32mIPV4 地址已更新: $New_IP\e[0m"
         Record_Type="A"
         update_IP
     fi
 
     if [ -n "$IPv6" ] && [ "$IPv6_IsLAN" != "1" ] && ! [[ `sys_ipv6` =~ `cat $IPv6_File` ]]; then
-        curl -s6m8 $api_v6 -k > $IPv6_File
-        New_IP=`cat $IPv6_File`
+        New_IP=$(curl -s6m8 $api_v6 -k) && echo $New_IP > $IPv6_File
         echo -e "\e[32mIPV6 地址已更新: $New_IP\e[0m"
         Record_Type="AAAA"
         update_IP
     fi
 }
+
+
+first_check
 
 # 每 3 分钟调用一次 check_ip_changes 函数,检查 IP 是否发生变化
 while true; do check_ip_changes && sleep 180; done
