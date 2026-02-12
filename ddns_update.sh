b7506addfb0fcffd11ed96c269890f9b417da39a
kskshaf <118548920+kskshaf@users.noreply.github.com>
2026-02-13 03:49:11 +0800

    Update
    使用配置文件保存配置，详见配置文件;
    去除脚本的"set -o errexit"，避免进程意外退出;
    网关测试改进;
    语法优化……
    (help by Gemini)

--- ddns_update.sh
+++ ddns_update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit
+# set -o errexit
 set -o nounset
 set -o pipefail
 #############################################################
@@ -11,69 +11,114 @@ set -o pipefail
 #
 #############################################################
 
-# 自行修改 Cloudflare_Zone_ID & Cloudflare_API_Tokens & Domain_Record
-Cloudflare_Zone_ID="type in zoneID"
-Cloudflare_API_Tokens="type in token"
-Domain_Record="ddns.example.com"
+# 使用单独的配置文件保存配置
+# 注意文件权限配置
+CONFIG_FILE="/path/to/ddns_update.conf"
 
-# IP API 地址
-api_v4="https://ipv4.ddnsip.cn"
-api_v6="https://ipv6.ddnsip.cn"
+# 检查配置文件
+if [ -f "$CONFIG_FILE" ]; then
+    source "$CONFIG_FILE"
+else
+    echo "请确认配置文件路径是否正确"
+    exit 1
+fi
 
-# 网关地址
-gateway='192.168.0.1'
+# 检查配置文件是否正确
+if [ -n "$Cloudflare_Zone_ID" ] && [ -n "$Cloudflare_API_Tokens" ] && [ -n "$Domain_Record" ]; then
+    :
+else
+    echo "请检查配置文件……"
+    exit 1
+fi
 
-# cloudflare 重连计数
 retry_count=0
-retry_limit=10
+tmp_ip_check_delay=$ip_check_delay
 
-# 网关测试
+# 使用ping测试网关
 gateway_test() {
-    ping -c 2 $gateway &> /dev/null
-    if [ $? -ne 0 ]; then
-        ping -c 5 $gateway &> /dev/null
-        if [ $? -ne 0 ]; then
+    if ! ping -c 2 $gateway &> /dev/null; then
+        if ! ping -c 5 $gateway &> /dev/null; then
             echo "网关无法连接"
-            exit 1
+            return 1
         fi
     fi
+    return 0
 }
 
-# 为了防止在 rc.local 中运行脚本时,网络尚未完全启动,造成获取 IP 失败,延迟 120 秒执行
-echo "延迟 120 秒执行"
-sleep 120
-gateway_test
+# 为了防止网络尚未完全启动而造成获取 IP 失败，延迟 $start_delay 秒执行
+echo "延迟$start_delay秒执行"
+sleep $start_delay
+
+# 等待网关ping通
+until gateway_test; do
+    ((retry_count++))
+    if((retry_count > retry_limit)); then
+        echo "网关仍然无法连接，退出进程"
+    fi
+
+    echo "等待网关就绪……"
+    sleep 30
+done
 
 # 获取路由器/光猫的公网 IP
-# 为防止大量请求 API , 使用两个文件保存旧的 IP 地址
-IPv4=$(curl -s4m8 $api_v4 -k)
-IPv6=$(curl -s6m8 $api_v6 -k)
+curl_ip() {
+    if [[ "${1}" == "4" ]]; then
+        IPv4=$(curl -s4m8 $api_v4 -k || echo "")
+    elif [[ "${1}" == "6" ]]; then
+        IPv6=$(curl -s6m8 $api_v6 -k || echo "")
+    fi
+}
 
 # 获取所有有效的 ipv4 地址
 sys_ipv4() {
     ip -4 -j addr show | jq -r '.[].addr_info[] | select(.scope == "global" and (.deprecated | not)) | .local'
 }
 
-# 获取有效时间最大的有效 ipv6 临时地址
-sys_ipv6() {
-    ip -6 -j addr show | jq -r '[.[].addr_info[] | select(.scope == "global" and .temporary and (.deprecated | not))] | max_by(.valid_life_time) | .local'
-}
-
 # 获取有效时间最大的有效 ipv6 地址
-# 无法获取临时地址时使用
+# 无法/无需获取临时地址时使用
 sys_ipv6_able() {
     ip -6 -j addr show | jq -r '[.[].addr_info[] | select(.scope == "global" and (.deprecated | not))] | max_by(.valid_life_time) | .local'
 }
 
+# 当开启 ipv6_temporary 时，获取有效时间最大的有效 ipv6 临时地址
+sys_ipv6() {
+    if $ipv6_temporary; then
+        ip -6 -j addr show | jq -r '[.[].addr_info[] | select(.scope == "global" and .temporary and (.deprecated | not))] | max_by(.valid_life_time) | .local'
+    else
+        sys_ipv6_able
+    fi
+}
+
+
+is_wan() {
+    if [[ "${1}" == "4" ]]; then
+        echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv4 地址为公网IP! \e[0m"
+        IPv4_IsLAN="0"
+    elif [[ "${1}" == "6" ]]; then
+        echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv6 地址为公网IP! \e[0m"
+        IPv6_IsLAN="0"
+    fi
+}
+
+
+is_lan() {
+    if [[ "${1}" == "4" ]]; then
+        echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv4 地址为私网IP! \e[0m"
+        IPv4_IsLAN="1"
+    elif [[ "${1}" == "6" ]]; then
+        echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv6 地址为私网IP! \e[0m"
+        IPv6_IsLAN="1"
+    fi
+}
+
+
 # 判断路由器/光猫拨号获取的 IP 地址是公网 IP 还是私网 IP , 如果 IPv4/IPv6 某项为空,说明是单栈
 detect_lan_or_wan() {
     if [ -n "$IPv4" ]; then
-        if ! [[ `sys_ipv4` =~ $IPv4 ]]; then
-            echo -e "\e[33m路由器/光猫 PPPoE 获取的 IPv4 地址为私网IP! \e[0m"
-            IPv4_IsLAN="1"
+        if [[ $(sys_ipv4) != *"$IPv4"* ]]; then
+            is_lan 4
         else
-            echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv4 地址为公网IP! \e[0m"
-            IPv4_IsLAN="0"
+            is_wan 4
         fi
     else
         echo -e "\e[32m网络错误，无法获取到外网 IPv4 地址! \e[0m"
@@ -81,12 +126,15 @@ detect_lan_or_wan() {
     fi
 
     if [ -n "$IPv6" ]; then
-        if [[ `sys_ipv6` != $IPv6 ]]; then
-            echo -e "\e[33m路由器/光猫 PPPoE 获取的 IPv6 地址为私网IP! \e[0m"
-            IPv6_IsLAN="1"
+        if [[ $(sys_ipv6) != $IPv6 ]]; then
+            # 无法获取临时地址时使用
+            if [[ $(sys_ipv6_able) != $IPv6 ]]; then
+                is_lan 6
+            else
+                is_wan 6
+            fi
         else
-            echo -e "\e[32m路由器/光猫 PPPoE 获取的 IPv6 地址为公网IP! \e[0m"
-            IPv6_IsLAN="0"
+            is_wan 6
         fi
     else
         echo -e "\e[32m无法获取到 IPv6 地址! \e[0m"
@@ -133,24 +181,21 @@ update_IP() {
     Record_Info=$(check_CF)
     Record_Info_Success=$(echo "$Record_Info" | jq -r ".success")
 
-    # 尝试重连10次，大于10次直接退出
-    while true; do
-        ((retry_count++))
-        if ((retry_count > $retry_limit)); then
-            echo -e "\e[31m重试次数大于$retry_limit次，退出进程\e[0m"
+    # 尝试重连$retry_limit次，大于直接退出
+    local cf_retry=0
+    while [[ $Record_Info_Success != "true" ]]; do
+        ((cf_retry++))
+        if ((cf_retry > retry_limit)); then
+            echo -e "\e[31m与 CloudFlare 连接重试次数大于$retry_limit次，退出进程\e[0m"
             exit 1
         fi
 
-        if [[ $Record_Info_Success != "true" ]]; then
-            echo -e "\e[31m与 Cloudflare 连接失败，重试中……\e[0m"
-            sleep 18
-            Record_Info=$(check_CF)
-            Record_Info_Success=$(echo "$Record_Info" | jq -r ".success")
-        else
-            echo -e "\e[31m与 Cloudflare 连接成功！\e[0m"
-            break
-        fi
+        echo -e "\e[31m与 Cloudflare 连接失败，重试中……($cf_retry)\e[0m"
+        sleep 18
+        Record_Info=$(check_CF)
+        Record_Info_Success=$(echo "$Record_Info" | jq -r ".success")
     done
+    echo -e "\e[31m与 Cloudflare 连接成功！\e[0m"
 
     Record_Id=$(echo "$Record_Info" | jq -r ".result[0].id")
     Record_Proxy=$(echo "$Record_Info" | jq -r ".result[0].proxied")
@@ -173,17 +218,20 @@ update_IP() {
     if [[ $Record_Info_No = "true" ]]; then
         echo -e "\e[32m域名IP无需更新 \e[0m"
         retry_count=0
+        ip_check_delay=$tmp_ip_check_delay
     elif [[ $Record_Info_Success = "true" ]]; then
         echo -e "\e[32m域名IP更新成功! \e[0m"
         retry_count=0
+        ip_check_delay=$tmp_ip_check_delay
     else
         echo -e "\e[31m域名IP更新失败，重试中……\e[0m"
-        update_IP
         ((retry_count++))
-        if ((retry_count > $retry_limit)); then
+        if ((retry_count > retry_limit)); then
             echo -e "\e[31m重试次数大于$retry_limit次，退出进程\e[0m"
             exit 1
         fi
+        # 当域名IP更新失败时，缩短check_ip_changes的循环执行时间到10秒
+        ip_check_delay=10
     fi
 }
 
@@ -206,34 +254,39 @@ check_ip_changes() {
     # 判断 IP 地址是否发生变化.如果IP发生变化,将新的IP地址写入文件,同时将IP赋值给New_IP变量,调用 update_IP 函数更新 IP
     # $IPv4/$IPv6 为空时说明路由器/光猫没有 IPv4/IPv6 地址,不予处理.
     # $IPv4_IsLAN/$IPv6_IsLAN 的值为 1 ,说明路由器/光猫获取的 IP 为内网 IP ,不予处理.
-    gateway_test
+    # 当开启 ipv6_detect_retry 时，尝试重新测试是否有公网 IPv6
+    gateway_test || return
 
-    if [[ $(sys_ipv6) != "null" ]] && [ "$IPv6_IsLAN" != "0" ]; then
-        IPv6=$(curl -s6m8 $api_v6 -k)
+    if $ipv6_detect_retry && [[ "$(sys_ipv6_able)" != "null" ]] && [ "$IPv6_IsLAN" != "0" ]; then
+        curl_ip 6
         echo -e "\e[32m尝试重新获取 IPV6 地址中：$IPv6\e[0m"
         detect_lan_or_wan
     fi
 
-    if [ -n "$IPv4" ] && [ "$IPv4_IsLAN" == "0" ] && ! [[ `sys_ipv4` =~ $IPv4 ]]; then
-        New_IP=$(curl -s4m8 $api_v4 -k)
-        IPv4=$New_IP
-        echo -e "\e[32mIPV4 地址已更新: $New_IP\e[0m"
-        Record_Type="A"
-        update_IP
+    if [ -n "$IPv4" ] && [ "$IPv4_IsLAN" == "0" ] && [[ "$(sys_ipv4)" != *"$IPv4"* ]]; then
+        curl_ip 4
+        if [ -n "$IPv4"]; then
+            New_IP=$IPv4
+            echo -e "\e[32mIPV4 地址已更新: $New_IP\e[0m"
+            Record_Type="A"
+            update_IP
+        else
+            echo "获取公网 IPv4 地址失败"
+            return 1
+        fi
     fi
 
 
-    if [ -n "$IPv6" ] && [ "$IPv6_IsLAN" == "0" ] && [[ `sys_ipv6` != $IPv6 ]]; then
-        New_IP=$(curl -s6m8 $api_v6 -k)
+    if [ -n "$IPv6" ] && [ "$IPv6_IsLAN" == "0" ] && [[ "$(sys_ipv6)" != "$IPv6" ]]; then
+        curl_ip 6
+        New_IP=$IPv6
         Sys_IPv6=$(sys_ipv6)
         Record_Type="AAAA"
 
         # 避免使用API获取IPV6地址错误时无法更新域名IP
         if [[ $Sys_IPv6 != "null" ]]; then
 
-            if [[ "$New_IP" == "$Sys_IPv6" ]]; then
-                IPv6=$New_IP
-            else
+            if [[ "$New_IP" != "$Sys_IPv6" ]]; then
                 IPv6=$Sys_IPv6
                 New_IP=$Sys_IPv6
                 echo -e "\e[32m使用API获取IPV6异常，以系统获取的IPV6为基准\e[0m"
@@ -242,7 +295,7 @@ check_ip_changes() {
             echo -e "\e[32mIPV6 地址已更新: $New_IP\e[0m"
             update_IP
 
-        elif [[ $(sys_ipv6_able) != "null" ]] && [[ $(sys_ipv6) == "null" ]]; then
+        elif [[ $(sys_ipv6_able) != "null" ]] && [[ $Sys_IPv6 == "null" ]]; then
             echo -e "\e[32m获取IPV6临时地址异常，使用系统可用的IPV6：$(sys_ipv6_able)\e[0m"
             IPv6=$(sys_ipv6_able)
             New_IP=$IPv6
@@ -250,7 +303,7 @@ check_ip_changes() {
         else
             echo -e "\e[32m获取IPV6异常\e[0m"
             ((retry_count++))
-            if ((retry_count > $retry_limit)); then
+            if ((retry_count > retry_limit)); then
                 echo -e "\e[31m重试次数大于$retry_limit次，退出进程\e[0m"
                 exit 1
             fi
@@ -259,8 +312,12 @@ check_ip_changes() {
     fi
 }
 
+curl_ip 4 && curl_ip 6
 detect_lan_or_wan
 first_check
 
-# 每 3 分钟调用一次 check_ip_changes 函数,检查 IP 是否发生变化
-while true; do check_ip_changes && sleep 180; done
+# 每 $ip_check_delay 秒调用一次 check_ip_changes 函数，检查 IP 是否发生变化
+while true; do
+    check_ip_changes
+    sleep $ip_check_delay
+done
